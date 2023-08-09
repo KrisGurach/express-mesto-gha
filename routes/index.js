70d3d81e11f94ebd792a5ef8df7b8b5d4615bd2f
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-08-09 21:41:42 +0600

    test fix

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,6 @@
 const router = require('express').Router();
 const { notFoundErrorCode, createError } = require('../helpers/errorHelpers');
 
-router.use((req, _, next) => {
-  req.user = {
-    _id: '64ce4ba9ad564cdf1b00faec',
-  };
-
-  next();
-});
-
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
 
